37d336114d7fe191e0c57e009e84544de11394b5
roma0000 <hishamtaha047@gmail.com>
2023-08-27 14:40:54 +0300

    Updates

---
+++ precache-manifest.8e97fffa5e67b389c172513a46e2bcd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61b11208451904a5b584",
+    "url": "/FollowUpLearn/static/css/main.5628d534.chunk.css"
+  },
+  {
+    "revision": "61b11208451904a5b584",
+    "url": "/FollowUpLearn/static/js/main.61b11208.chunk.js"
+  },
+  {
+    "revision": "2b7882079dc909c4d862",
+    "url": "/FollowUpLearn/static/css/1.c17915ba.chunk.css"
+  },
+  {
+    "revision": "2b7882079dc909c4d862",
+    "url": "/FollowUpLearn/static/js/1.2b788207.chunk.js"
+  },
+  {
+    "revision": "3631c0eed89f92733d4a",
+    "url": "/FollowUpLearn/static/js/runtime~main.3631c0ee.js"
+  },
+  {
+    "revision": "5d06e6f8318da271deddf500a985e086",
+    "url": "/FollowUpLearn/index.html"
+  }
+];
